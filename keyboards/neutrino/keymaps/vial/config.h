51ff6d7549ccaec2d5b48fecad078258b5f4133c
Kevin Wang <9001706+kevjawang@users.noreply.github.com>
2023-08-11 23:00:29 -0500

    Add neutrino handwire

---
+++ keyboards/neutrino/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC6, 0xDF, 0x38, 0xE9, 0x4E, 0x81, 0xF7, 0x2B}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
