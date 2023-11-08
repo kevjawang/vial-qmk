6435fc126298e9906a234a06762dd31a3af348b5
Kevin Wang <9001706+kevjawang@users.noreply.github.com>
2023-11-08 13:04:35 -0600

    NK65:base vial port

--- keyboards/novelkeys/nk65/config.h
+++ keyboards/novelkeys/nk65/config.h
@@ -38,17 +38,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_BACKLIGHT_DISABLE_AFTER_TIMEOUT 0
 
 // the default brightness
-#define RGB_BACKLIGHT_BRIGHTNESS 255
+#define RGB_BACKLIGHT_BRIGHTNESS 0
 
 // the default effect (RGB test)
-#define RGB_BACKLIGHT_EFFECT 6
+#define RGB_BACKLIGHT_EFFECT 0
 
 // the default effect speed (0-3)
 #define RGB_BACKLIGHT_EFFECT_SPEED 0
 
 // the default color1 and color2
-#define RGB_BACKLIGHT_COLOR_1 { .h = 0, .s = 255 }
-#define RGB_BACKLIGHT_COLOR_2 { .h = 127, .s = 255 }
+#define RGB_BACKLIGHT_COLOR_1 { .h = 0, .s = 0 }
+#define RGB_BACKLIGHT_COLOR_2 { .h = 0, .s = 0 }
 
 #define DRIVER_COUNT 2
 #define RGB_MATRIX_LED_COUNT 128
---
+++ keyboards/novelkeys/nk65/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA7, 0x2E, 0x73, 0xA9, 0x63, 0x45, 0xF3, 0x7D}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
